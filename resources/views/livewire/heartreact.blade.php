6124ab0f54ea062c0420683465b49862b133d943
DiogoDuart3 <31131606+DiogoDuart3@users.noreply.github.com>
2024-05-18 10:16:11 +0100

    Note show & heart count

---
+++ resources/views/livewire/heartreact.blade.php
@@ -0,0 +1,25 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Models\Note;
+
+new class extends Component {
+    public Note $note;
+    public $heartCount;
+
+    public function mount(Note $note)
+    {
+        $this->note = $note;
+        $this->heartCount = $note->heart_count;
+    }
+
+    public function heart()
+    {
+        $this->note->increment('heart_count');
+        $this->heartCount = $this->note->heart_count;
+    }
+}; ?>
+
+<div>
+    <x-button xs wire:click='heart' rose icon="heart" spinner="heart">{{ $heartCount }}</x-button>
+</div>
--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -39,7 +39,8 @@ new class extends Component {
             @foreach ($notes as $note)
                 <x-card wire:key='{{ $note->id }}'>
                     <div class="flex justify-between">
-                        <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
+                        <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500"
+                            href="{{ route('notes.edit', $note) }}" wire:navigate>
                             {{ $note->title }}
                         </a>
                         <p class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('d/m/Y') }}
@@ -53,7 +54,7 @@ new class extends Component {
                     <div class="flex items-end justify-between mt-4 space-x-1">
                         <p class="text-xs">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
                         <div>
-                            <x-button.circle icon="eye" href="{{ route('notes.edit', $note) }}" wire:navigate />
+                            <x-button.circle icon="eye" href="{{ route('notes.show', $note) }}" wire:navigate />
                             <x-button.circle icon="trash" wire:click="delete('{{ $note->id }}')" />
                         </div>
                     </div>
